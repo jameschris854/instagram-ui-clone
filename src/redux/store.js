8638d0c296e9a91d82b88e7749b180b706abe8c8
James christopher <68394236+jameschris854@users.noreply.github.com>
2021-06-14 17:15:28 +0530

    prod

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
 
 import {createStore,applyMiddleware} from 'redux';
-// import logger from 'redux-logger'
+import logger from 'redux-logger'
 import rootReducer from './rootReducer'
 import { persistStore } from 'redux-persist'
 
 
-const middlewares = []
+const middlewares = [logger]
 
 
 export const store = createStore(rootReducer,applyMiddleware(...middlewares))
